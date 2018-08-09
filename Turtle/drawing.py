94bef355d018300a8c6234585bb21335c0f0cb0b
Arurie <alexarcra@gmail.com>
2018-08-09 18:11:10 +0100

    Don't do this while driving!

--- Turtle/drawing.py
+++ Turtle/drawing.py
@@ -1,19 +1,29 @@
 import turtle
 
-def draw_square():
+count = 0
+
+def draw_square(turtles):
+    for i in range(1, 5):
+        turtles.forward(100)
+        turtles.right(90)
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("blue")
-    
+
     clarence = turtle.Turtle()
-    clarence.forward(100)
-    clarence.right(90)
-    clarence.forward(100)
-    clarence.right(90)
-    clarence.forward(100)
-    clarence.right(90)
-    clarence.forward(100)
-    clarence.right(90)
+    clarence.color("red")
+    clarence.shape("turtle")
+    for i in range(1, 37):
+        draw_square(clarence)
+        clarence.right(10)
+
+    maria = turtle.Turtle()
+    maria.circle(100)
+    maria.color("purple")
+    maria.shape("arrow")
 
     window.exitonclick()
 
 draw_square()
+draw_circle()
